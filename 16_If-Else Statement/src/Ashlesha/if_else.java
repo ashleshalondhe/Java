6383c37e445f9a66b8026c009baa863aa8e2a0a9
Ashlesha Londhe <140420012+ashleshalondhe@users.noreply.github.com>
2024-01-04 19:12:33 +0530

    Add files via upload
    All about Conditional: If-else statement and how to use the if-else ladder

---
+++ 16_If-Else Statement/src/Ashlesha/if_else.java
@@ -0,0 +1,41 @@
+/*Syntax/*
+/* if (condition-to-be-checked) {
+	statements-if-condition-true;
+}
+else {
+	statements-if-condition-false;
+} */
+
+package Ashlesha;
+
+public class if_else {
+
+	public static void main(String[] args) {
+		
+		int a = 29;
+		if (a>18) {
+			System.out.println("You can drive");
+		}
+		else{
+		       System.out.println("You are underage!");
+		}
+
+	}
+
+}
+/*If-else ladder/*
+ /* if (condition1) {
+
+            //Statements;
+
+else if {
+
+            // Statements;
+
+}
+
+else {
+
+            //Statements
+
+} */
