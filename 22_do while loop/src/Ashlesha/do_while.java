9940f645f86cb1d4f3931b90027351b569f6b135
Ashlesha Londhe <140420012+ashleshalondhe@users.noreply.github.com>
2024-01-06 22:18:33 +0530

    Add files via upload

---
+++ 22_do while loop/src/Ashlesha/do_while.java
@@ -0,0 +1,26 @@
+package Ashlesha;
+
+public class do_while {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First loop: ");
+		int a = 0;
+		do {
+			System.out.println(a);
+			a++;
+		} while(a<5);
+		
+		System.out.println("Second loop: ");
+		
+		int b = 1;
+        do {
+            System.out.println(b);
+            b++;
+        } while(b<=45);
+		
+		
+
+	}
+
+}
