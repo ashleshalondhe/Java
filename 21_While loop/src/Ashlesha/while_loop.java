a1a4f28282c887ef2c173b21701de7679a80b261
Ashlesha Londhe <140420012+ashleshalondhe@users.noreply.github.com>
2024-01-06 21:27:34 +0530

    Add files via upload

---
+++ 21_While loop/src/Ashlesha/while_loop.java
@@ -0,0 +1,23 @@
+package Ashlesha;
+
+public class while_loop {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Using Loops: ");
+	    int i = 1;
+		
+		while(i<=3) {
+		 System.out.println(i);
+		 i++;
+		}
+		System.out.println("Finish Running While loop!");
+		
+		while(true) {
+			System.out.println("Infinite while loop!");
+		}
+
+	}
+
+}
+
