ab3c77170f03f57a1e9bc811b2fd9ee8a2d08d6f
Ashlesha Londhe <140420012+ashleshalondhe@users.noreply.github.com>
2024-01-14 23:28:43 +0530

    Add files via upload

---
+++ 24_break/src/Ashlesha/java_break.java
@@ -0,0 +1,24 @@
+//Example to demonstrate the use of break inside a for loop :
+
+package Ashlesha;
+
+public class java_break {
+
+	public static void main(String[] args) {
+		  
+			    //using for loop  
+		
+			    for(int i=10;i>0;i--){  
+			        if(i==7)
+			        {  
+			        	
+			            break;   //break the loop
+			        }  
+			       
+			        System.out.println(i);  
+			    }  
+			}  
+			}  
+
+
+	
